89fc30ecb9166379927934550cdb29d83352db4f
Joshua Nahum <josh@nahum.us>
2012-05-24 09:54:41 -0700

    added colicin work from Dropbox

---
+++ colicin_sim.py
@@ -0,0 +1,31 @@
+"""
+Simulation: 
+1) colicin-immunity interaction module 
+	
+2) Bacterial Population module 
+"""
+
+from colicin_immunity import Colicin
+from colicin_immunity import Immunity
+
+if __name__ == '__main__':
+	print 'start'
+
+	c = Colicin()
+	i = Immunity()
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	print 'done' 
+	
