386c399efdea6519ea626ce96a11929dddf02948
Joshua Nahum <josh@nahum.us>
2012-05-29 11:57:20 -0700

    changed bitstring's string input

--- bitstring.py
+++ bitstring.py
@@ -1,21 +1,26 @@
 """
 This module implements a crude Bitstring with boolean values
-in python tuples.
+in python tuples. Bitstring instances are immutable.
 """
 
 class Bitstring(object):
 
-    def __init__(self, string_rep=""):
-        self.value = tuple(char == "1" for char in reversed(string_rep))
+    def __init__(self, iterable=None):
+        if not iterable:
+            iterable = []
+        elif isinstance(iterable, str):
+            self._value = tuple(char == "1" for char in iterable)
+        else:
+            self._value = tuple(bool(char) for char in iterable)
 
     def __len__(self):
-        return len(self.value)
+        return len(self._value)
 
     def __getitem__(self, key):
-        return self.value[key]
+        return self._value[key]
 
     def __iter__(self):
-        return iter(self.value)
+        return iter(self._value)
 
     def __eq__(self, other):
         if type(self) != type(other):
@@ -26,7 +31,7 @@ class Bitstring(object):
                 in zip(self, other))
 
     def __hash__(self):
-        return hash(self.value)
+        return hash(self._value)
 
     def __repr__(self):
         def bool_to_str(boolean):
@@ -34,11 +39,9 @@ class Bitstring(object):
                 return "1"
             return "0"
 
-        value_as_string_little_endian = "".join(
+        value_as_string = "".join(
                 bool_to_str(pos) for pos in self)
-        value_as_string = value_as_string_little_endian[::-1]
-        return "{}('{}')".format(self.__class__.__name__, value_as_string)
-
+        return "{}({!r})".format(self.__class__.__name__, value_as_string)
 
     def hamming_distance(self, other):
         return len(tuple(None for (self_pos, other_pos)
