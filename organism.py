7ed7d47bf46e60987f0e12a649ee3f3ff863bbf1
Joshua Nahum <josh@nahum.us>
2012-05-29 18:18:31 -0700

    added immune_to and is_self_toxic methods

--- organism.py
+++ organism.py
@@ -6,4 +6,23 @@ class Organism(object):
     """
     Holds colicin genes and immunity genes.
     """
-    pass
+
+    def __init__(self, colicins, immunities):
+        self.colicins = colicins
+        self.immunities = immunities
+
+    def is_immune_to(self, colicin):
+        """
+        Returns true if one (or more) of the orgs Immunity genes
+        can bind to the colicin.
+        """
+        return any(immunity.can_bind(colicin) for immunity
+                in self.immunities)
+
+    def is_self_toxic(self):
+        """
+        Returns true if one or more of the colicins produced by
+        self can kill self.
+        """
+        return any(not self.is_immune_to(colicin) for colicin
+                in self.colicins)
