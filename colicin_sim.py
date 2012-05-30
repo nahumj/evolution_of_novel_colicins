500a94aefa1682cbe3557ba1a9c4dcc69a3129af
Joshua Nahum <josh@nahum.us>
2012-05-29 23:52:10 -0700

    made demo evolution sim

--- colicin_sim.py
+++ colicin_sim.py
@@ -7,11 +7,30 @@ Simulation:
 
 from colicin_immunity import Colicin
 from colicin_immunity import Immunity
+from organism import Organism
+from population import Population
+
+
+def all_colicins(population):
+    for org in population:
+        for colicin in org.colicins:
+            yield colicin
+
+
+def average_colicin_id(population):
+    ids = [col.id for col in all_colicins(population)]
+    return sum(ids) / float(len(ids))
+
 
 if __name__ == '__main__':
-    print 'start'
+    print('start')
+    org = Organism([Colicin(0)], [Immunity(0, 5)])
+    pop = Population(org for _ in range(50))
 
-    c = Colicin()
-    i = Immunity()
+    for gen in range(1000):
+        pop.advance_generation()
+        average_id = average_colicin_id(pop)
+        if gen % 100 == 0:
+            print("{}\t{}".format(gen, average_id))
 
-    print 'done'
+    print('done')
