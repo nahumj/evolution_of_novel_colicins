efc91a4751cc51ed403eb2efff46e34529181fa3
Joshua Nahum <josh@nahum.us>
2012-05-30 14:04:39 -0700

    removed redundant all_colicins function

--- colicin_sim.py
+++ colicin_sim.py
@@ -12,21 +12,14 @@ from organism import Organism
 from population import Population
 
 
-def all_colicins(population):
-    for org in population:
-        for colicin in org.colicins:
-            yield colicin
-
-
 def average_colicin_id(population):
-    ids = [col.id for col in all_colicins(population)]
+    ids = [col.id for col in population.colicins_produced()]
     return sum(ids) / float(len(ids))
 
 
-if __name__ == '__main__':
-    print('start')
+def main():
     org = Organism([Colicin(0)], [Immunity(0, 5)])
-    pop = Population(org for _ in range(50))
+    pop = Population(org for _ in range(5))
 
     for gen in range(1000):
         pop.advance_generation()
@@ -34,4 +27,7 @@ if __name__ == '__main__':
         if gen % 100 == 0:
             print("{}\t{}".format(gen, average_id))
 
+if __name__ == '__main__':
+    print('start')
+    main()
     print('done')
