36be49af09decaf1fd1bb2b0909b89f0c916e6b2
Joshua Nahum <josh@nahum.us>
2012-05-30 22:46:50 -0700

    made colicin bitstring

---
+++ colicin_bitstring.py
@@ -0,0 +1,12 @@
+"""
+a colicin is represented by a bitstring
+immunity is represented by another bitstring
+colicin binding to immunity is represented by the
+hamming distance being less than
+a proportion
+"""
+from colicin import Colicin
+
+
+class ColicinBitstring(Colicin):
+    pass
---
+++ test_colicin_bitstring.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase as TC
+
+from colicin_bitstring import ColicinBitstring
+from bitstring import Bitstring
+
+
+class TestColicinBitstring(TC):
+    def setUp(self):
+        self.value = Bitstring("10101")
+        self.c = ColicinBitstring(self.value)
+
+    def test_init(self):
+        #write what we want to check
+        self.assertEqual(self.value, self.c.id)
+
+    def test_mutate(self):
+        d = self.c.mutate()
+        self.assertNotEqual(self.c.id, d.id)
