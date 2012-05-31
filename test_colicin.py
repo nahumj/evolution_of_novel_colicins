2d4c042cc92904596ba696f754a80ec644bf5752
Joshua Nahum <josh@nahum.us>
2012-05-30 19:15:45 -0700

    broke apart colicin_immunity tests

---
+++ test_colicin.py
@@ -0,0 +1,19 @@
+import unittest
+from unittest import TestCase as TC
+
+from colicin_immunity import Colicin
+
+
+class TestColicin(TC):
+    #inherits from TestCase
+    def setUp(self):
+        self.value = 7
+        self.c = Colicin(self.value)
+
+    def test_init(self):
+        #write what we want to check
+        self.assertEqual(self.value, self.c.id)
+
+    def test_mutate(self):
+        d = self.c.mutate()
+        self.assertNotEqual(self.c.id, d.id)
--- test_immunity.py
+++ test_immunity.py
@@ -1,23 +1,8 @@
 import unittest
 from unittest import TestCase as TC
 
-from colicin_immunity import Colicin
 from colicin_immunity import Immunity
-
-
-class TestColicin(TC):
-    #inherits from TestCase
-    def setUp(self):
-        self.value = 7
-        self.c = Colicin(self.value)
-
-    def test_init(self):
-        #write what we want to check
-        self.assertEqual(self.value, self.c.id)
-
-    def test_mutate(self):
-        d = self.c.mutate()
-        self.assertNotEqual(self.c.id, d.id)
+from colicin_immunity import Colicin
 
 
 class TestImmunity(TC):
