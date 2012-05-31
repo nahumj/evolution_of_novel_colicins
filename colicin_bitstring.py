016497122ac5bed71a8e2d88dc99c64dacd2a879
Joshua Nahum <josh@nahum.us>
2012-05-31 01:06:30 -0700

    removed inheritance

--- colicin_bitstring.py
+++ colicin_bitstring.py
@@ -5,8 +5,25 @@ colicin binding to immunity is represented by the
 hamming distance being less than
 a proportion
 """
-from colicin import Colicin as ColicinInt
+from mixins import Duplicatable, Printable, Equalable
+from mutate import mutate
 
 
-class Colicin(ColicinInt):
-    pass
+class Colicin(Duplicatable, Printable, Equalable):
+
+    def __init__(self, id):
+        self.id = id
+
+    def duplicate(self):
+        duplicate = Colicin(self.id)
+        return duplicate
+
+    def mutate(self):
+        """
+        Returns a Colicin with an id shifted by one
+        """
+        new_id = mutate(self.id)
+        return Colicin(new_id)
+
+    def __hash__(self):
+        return hash(self.id)
--- immunity_bitstring.py
+++ immunity_bitstring.py
@@ -2,12 +2,22 @@
 a colicin is represented by a single bitstring
 immunity is represented by another bitstring
 """
+from mixins import Duplicatable, Printable, Equalable
 from mutate import mutate
-from immunity import Immunity as ImmunityInt
 
 
-class Immunity(ImmunityInt):
+class Immunity(Duplicatable, Printable, Equalable):
+    def __init__(self, id, binding_range):
+        self.id = id
+        self.binding_range = binding_range
 
     def can_bind(self, colicin):
         dist = colicin.id.hamming_distance(self.id)
         return dist <= self.binding_range
+
+    def mutate(self):
+        """
+        Returns an immunity instance shifted by one
+        (Binding range unchanged)
+        """
+        return Immunity(mutate(self.id), self.binding_range)
